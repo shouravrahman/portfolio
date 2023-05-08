a87907d19e318343deba209107b12d88a698d166
shouravrahman <shouravrahman006@gmail.com>
2023-05-09 01:31:42 +0600

    layout component for universal spacing

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = ({ children, className = "" }) => {
+	return (
+		<div className={`w-full h-full z-0 inline-block p-32 ${className}`}>
+			{children}
+		</div>
+	);
+};
+
+export default Layout;
