82a578c2316ecb8791dadd2d52f3d692b758b788
shouravrahman <shouravrahman006@gmail.com>
2023-05-10 12:40:55 +0600

    logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Logo = () => {
+	return (
+		<div className='flex items-center justify-center '>
+			<img
+				className='w-[100px] h-[100px]'
+				src='/images/svgs/logo.svg'
+				alt='logo'
+			/>
+		</div>
+	);
+};
+
+export default Logo;
